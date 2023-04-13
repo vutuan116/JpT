8d2448122b9c1e7bbf204f2705fd73b5aebdfd77
cmcnvtuan11 <cmc.nvtuan11@partner.samsung.com>
2023-04-13 16:46:10 +0700

    commit

--- js/gramIndex.js
+++ js/gramIndex.js
@@ -1,13 +1,15 @@
 var gramEditor = [];
 var grammarIndex = 0;
+var chaSpec = "０１２３４５６７８９ー・　（）＆．＋？";
+var chaNorm = "0123456789-/ ()&.+?";
 
 $(document).ready(function () {
     genGrammar();
 });
 function genGrammar() {
     grammarIndex++;
-    var html = 
-        `<div class="row grammar gra_in_1 ${grammarIndex%2==0?'even':''}">
+    var html =
+        `<div class="row grammar gra_in_1 ${grammarIndex % 2 == 0 ? 'even' : ''}">
             <h4>Grammar ${grammarIndex}: </h4>
             <div class="row mb-3">
                 <h5 class=" col-auto gra_label text-end form-label mb-0 pt-1">Label:</h5>
@@ -26,7 +28,7 @@ function genGrammar() {
                 <textarea class="col form-control" rows="4" id="exam_gra_in_${grammarIndex}"></textarea>
             </div>
         </div>`;
-    
+
     $("#grammar_div").append(html);
 }
 
@@ -46,9 +48,42 @@ function createGrammar() {
         dataTemp.Mean = $("#mean_gra_in_" + index).val();
         dataTemp.Gram = $("#gram_gra_in_" + index).val();
         dataTemp.Example = $("#exam_gra_in_" + index).val();
-        grammar.Data.push(dataTemp);
+        if (dataTemp.Label != "") {
+            grammar.Data.push(dataTemp);
+        }
         index++;
     }
+    var txtData = JSON.stringify(grammar);
+    for (i = 0; i < chaSpec.length; i++) {
+        txtData = txtData.replaceAll(chaSpec[i], chaNorm[i]);
+    }
+    $("#grammarData").val(txtData);
+}
 
-    $("#grammarData").val(JSON.stringify(grammar));
+function convertGrammar() {
+    if ($("#grammarData").val() == "") {
+        return;
+    }
+
+    var txtData = $("#grammarData").val();
+    for (i = 0; i < chaSpec.length; i++) {
+        txtData = txtData.replaceAll(chaNorm[i], chaSpec[i]);
+    }
+
+    var jsonData = JSON.parse(txtData);
+
+    $("#lesson").val(jsonData.Lesson);
+    $("#level").val(jsonData.Level);
+
+    for (i = 0; i < jsonData.Data.length; i++) {
+        if ($("#label_gra_in_" + (i + 1)).length==0){
+            genGrammar();
+        }
+
+        $("#label_gra_in_" + (i + 1)).val(jsonData.Data[i].Label);
+        $("#mean_gra_in_" + (i + 1)).val(jsonData.Data[i].Mean);
+        $("#gram_gra_in_" + (i + 1)).val(jsonData.Data[i].Gram);
+        $("#exam_gra_in_" + (i + 1)).val(jsonData.Data[i].Example);
+    }
+
 }
